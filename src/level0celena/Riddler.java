63ebef6fe6b512b0c12bfde965ab8b249c3c84b2
celenabanuelos <league@Mac-mini-3.local>
2017-05-27 11:25:54 -0700

    elena

---
+++ src/level0celena/Riddler.java
@@ -0,0 +1,12 @@
+package level0celena;
+
+import javax.swing.JOptionPane; class Riddler {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog(" what has to be open before you can use it?");
+	    JOptionPane.showMessageDialog(null, "an egg");
+	    JOptionPane.showInputDialog(null, " what can you cath but not throw?");
+	    JOptionPane.showMessageDialog(null, "a cold");
+	    JOptionPane.showInputDialog("what is full of holes but can still hold water?");
+	}
+	
+}
---
+++ src/level0celena/warmup.java
@@ -0,0 +1,13 @@
+ package level0celena;
+
+import javax.swing.JOptionPane;
+
+public class warmup {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "hi my name celena ");
+    JOptionPane.showMessageDialog(null, "i like pizza");
+    JOptionPane.showMessageDialog(null, "I like puppys");
+    JOptionPane.showMessageDialog(null, "i love tacos);
+    JOptionPane.showMessageDialog(null, "i like dolphins");
+}
+}
