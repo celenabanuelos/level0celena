b9b13df6073a2b431dc763031c62f724bb7fbb4f
celenabanuelos <league@FunkyMini.local>
2017-08-26 11:31:30 -0700

    unbirthday

--- src/level0celena/Unbirthday.java
+++ src/level0celena/Unbirthday.java
@@ -3,13 +3,13 @@ package level0celena;
 import javax.swing.JOptionPane;
 
 public class Unbirthday {
-public static void main(String[] args) {
-	String birthday= JOptionPane.showInputDialog("when's your birthday");
-	if (birthday.equals("September 29")){
-		JOptionPane.showMessageDialog(null, "happy birthday!!!!!!!!");
-		
-	}else {
-		JOptionPane.showMessageDialog(null, "happy unbirthday");
+	public static void main(String[] args) {
+		String birthday = JOptionPane.showInputDialog("when's your birthday");
+		if (birthday.equals("September 29")) {
+			JOptionPane.showMessageDialog(null, "happy birthday!!!!!!!!");
+
+		} else {
+			JOptionPane.showMessageDialog(null, "happy unbirthday");
+		}
 	}
-}
 }
