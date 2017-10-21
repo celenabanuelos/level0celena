b5de4ce9292439294b1e1ddaef3c179b0e06c00a
joshpryor <league@Admins-Mac-mini-2.local>
2017-10-21 11:31:02 -0700

    10-21-17
    finish checkpoint

---
+++ src/Checkpoint.java
@@ -0,0 +1,4 @@
+
+public class Checkpoint {
+public static
+}
---
+++ src/YearsAlive.java
@@ -0,0 +1,9 @@
+
+public class YearsAlive {
+	public static void main(String[] args){
+		for(int year=2006;year<=2017;year++){
+			System.out.println("i was alive in the year " +year);
+			
+		}
+	}
+}
